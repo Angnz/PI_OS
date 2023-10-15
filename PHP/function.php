7c451013a929ba6838f80e659dcbf5f46491a873
Ángelo <137942818+Angnz@users.noreply.github.com>
2023-10-15 10:16:29 +0200

    Update function.php

--- PHP/function.php
+++ PHP/function.php
@@ -1,22 +1,22 @@
 /*My Code for online shop - To add in functions.php in wordpress*/
 
 
-function petitaideas_os_css() {
+function function_css() {
     // Enqueue the CSS file
-    wp_enqueue_style('petitaideas_os_style', '/wp-includes/css/custom-css/PetitaideasOS_style.css');
+    wp_enqueue_style('function_css', '/wp-includes/css/custom-css/yourcss.css');
 }
 add_action('wp_enqueue_scripts', 'petitaideas_os_css');
 
 
-function petitaideas_os_js() {
+function function_css() {
     // Check if the current page is the "test" page
-    if (is_page('test')) {
+    if (is_page('yourpageid')) {
         // Enqueue the script only if the current page is "test"
-        wp_register_script('mi_script', '/wp-includes/js/custom-scripts/PetitaideasOS_2.js', array('jquery'), '1', true );
-        wp_enqueue_script('mi_script');
+        wp_register_script('function_css', '/wp-includes/js/custom-scripts/yourjs.js', array('jquery'), '1', true );
+        wp_enqueue_script('function_css');
     }
 }
-add_action('wp_enqueue_scripts','petitaideas_os_js');
+add_action('wp_enqueue_scripts','function_css');
 
 
 /*My Code for online shop END*/
